fa88184153deecf267f994e687585f4250ce6d9b
mevanoxx <mevenjoyer@proton.me>
2025-06-03 13:27:05 -0400

    2.2.15 rebase

--- bundle/src/lib.rs
+++ bundle/src/lib.rs
@@ -68,11 +68,3 @@ pub struct SanitizedBundle {
     pub transactions: Vec<RuntimeTransaction<SanitizedTransaction>>,
     pub slot: u64,
 }
-
-// pub fn derive_bundle_id_from_sanitized_transactions(
-//     transactions: &[RuntimeTransaction<SanitizedTransaction>],
-// ) -> String {
-//     let mut hasher = Sha256::new();
-//     hasher.update(transactions.iter().map(|tx| tx.signature()).join(","));
-//     format!("{:x}", hasher.finalize())
-// }
