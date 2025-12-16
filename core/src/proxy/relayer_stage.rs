ad73eb9cff05f92d2d9b968a0017c0cda8f20a48
cavemanloverboy <caveycool@gmail.com>
2025-12-16 17:43:28 +0000

    fix: tune be/relay grpc clients

--- core/src/proxy/relayer_stage.rs
+++ core/src/proxy/relayer_stage.rs
@@ -201,9 +201,9 @@ impl RelayerStage {
             })?
             .tcp_nodelay(true)
             .tcp_keepalive(Some(Duration::from_secs(60)))
-            .keepalive_while_idle(true)
-            .http2_keep_alive_interval(Some(Duration::from_secs(30)))
-            .http2_keep_alive_timeout(Duration::from_secs(10));;
+            .keep_alive_while_idle(true)
+            .keep_alive_timeout(Duration::from_secs(10))
+            .http2_keep_alive_interval(Duration::from_secs(30));
         if local_relayer_config.relayer_url.starts_with("https") {
             backend_endpoint = backend_endpoint
                 .tls_config(tonic::transport::ClientTlsConfig::new())
